5747fc1b0b7a4c06698848b3f8cd611fec35b4c8
Luca Graves <Lgrave264@west-mec.local>
2021-09-16 13:31:57 -0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Errorloading from './components/Errorloading';
+
+function App(){
+    
+    return(
+        <main>
+            <section className='container'>
+                <Errorloading />
+            </section>
+        </main>
+    )
+}
+
+export default App
